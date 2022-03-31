cd79bbc0d58817600d2206dad1fb84a53a18fd60
Olivier Cailloux <olivier.cailloux@gmail.com>
2022-03-31 18:19:45 +0200

    Simpl (still pbl)

--- src/main/java/io/github/oliviercailloux/jquestions/Startup.java
+++ src/main/java/io/github/oliviercailloux/jquestions/Startup.java
@@ -1,15 +1,6 @@
 package io.github.oliviercailloux.jquestions;
 
-import com.google.common.collect.ImmutableSet;
-import com.google.common.io.Resources;
-import io.github.oliviercailloux.jaris.collections.ImmutableCompleteMap;
-import io.github.oliviercailloux.jaris.credentials.CredentialsReader;
-import io.github.oliviercailloux.jaris.credentials.CredentialsReader.ClassicalCredentials;
-import io.github.oliviercailloux.jquestions.entities.Question;
-import io.github.oliviercailloux.jquestions.entities.User;
 import io.quarkus.runtime.StartupEvent;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
@@ -26,30 +17,8 @@ public class Startup {
 	@Inject
 	EntityManager em;
 
-	@Inject
-	UserService userService;
-
-	@Inject
-	QuestionService questionService;
-	@Inject
-	ExamService examService;
-
-	@Inject
-	QuestionParser questionParser;
-
 	@Transactional
-	public void loadAtStartup(@Observes StartupEvent evt) throws IOException {
+	public void loadAtStartup(@Observes StartupEvent evt) {
 		LOGGER.info("Loading at startup due to {}.", evt);
-
-		final ImmutableCompleteMap<ClassicalCredentials, String> credentials = CredentialsReader.classicalReader()
-				.getCredentials();
-		userService.persist(new User(credentials.get(ClassicalCredentials.API_USERNAME),
-				credentials.get(ClassicalCredentials.API_PASSWORD), User.ADMIN_ROLE));
-
-		final String examAsciiDoc = Resources.toString(getClass().getResource("Exam.adoc"), StandardCharsets.UTF_8);
-		final ImmutableSet<Question> questions = questionParser.parseQuestions(examAsciiDoc);
-		questions.forEach(questionService::persist);
-
-		examService.persist(questions.asList(), "zepw");
 	}
 }
